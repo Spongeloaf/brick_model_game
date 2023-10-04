b7201600faf35667e5de85dee963360adfeee056
Spongeloaf <peter.v@live.ca>
2023-10-03 20:15:12 -0400

    Revert "Rename playercontroller to playerinterface for readability"
    This reverts commit 028571f5168b902251b536b2dd145115cf673dc5.

--- source/playerController/PlayerController.cs
+++ source/playerController/PlayerController.cs
@@ -3,7 +3,7 @@ using Godot.Collections;
 using System;
 using System.Collections.Generic;
 
-public partial class PlayerInterface : Node3D
+public partial class PlayerController : Node
 {
   private float screenRayDepth = 100f;
   private GameManager m_gameManager;
