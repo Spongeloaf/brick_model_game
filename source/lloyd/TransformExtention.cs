0c9bc2e2d6106305396f171bf686afcd019a36b1
Spongeloaf <peter.v@live.ca>
2023-10-29 10:57:46 -0400

    Starting work on Lloyd

---
+++ source/lloyd/TransformExtention.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using static System.Numerics.Matrix4x4;
+using Godot;
+using System;
+
+namespace LDraw
+{
+	public static class TransformExtention
+	{
+		public static void ApplyLocalTRS(this Transform3D tr, System.Numerics.Matrix4x4 trs)
+		{
+			throw new NotImplementedException();
+
+			//tr.localPosition = trs.ExtractPosition();
+			//tr.localRotation = trs.ExtractRotation();
+			//tr.localScale = trs.lossyScale;
+		}
+		
+		public static System.Numerics.Matrix4x4 ExtractLocalTRS(this Transform3D tr)
+		{
+      throw new NotImplementedException();
+      // Unity.Matrix4x4.TRS() Creates a translation, rotation and scaling matrix
+      // return System.Numerics.Matrix4x4.TRS(tr.localPosition, tr.localRotation, tr.localScale);
+		}
+
+		public static void LocalReflect(this Transform3D tr, Vector3 planeNormal)
+		{
+      throw new NotImplementedException();
+      //var trs = tr.ExtractLocalTRS();
+      //var reflected = trs.HouseholderReflection(planeNormal);
+      //tr.ApplyLocalTRS(reflected);
+    }
+	}
+}
