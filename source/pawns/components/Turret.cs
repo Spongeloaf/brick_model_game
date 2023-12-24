f2a7da06cf5c9e4c85fb3c5838b5f5f94882ce96
Spongeloaf <peter.v@live.ca>
2023-12-24 12:47:38 -0500

    Built a turret in a scene to research a good structure for the models

---
+++ source/pawns/components/Turret.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+[GlobalClass, Icon("res://source/pawn/pawn.svg")]
+public partial class Turret : Area3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ source/pawns/vehicle/VehicleController.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+
+[GlobalClass, Icon("res://source/pawn/pawn.svg")]
+public partial class VehicleController : RigidBody3D
+{
+    Node3D m_turret;
+
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+
+    }
+}
